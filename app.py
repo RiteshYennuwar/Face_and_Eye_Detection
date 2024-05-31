0fd56fa50e91a9af0dbbfd72b5c8f31c91df5c25
RiteshYennuwar <yennuwarritesh@hotmail.com>
2024-05-31 11:11:41 +0530

    added face and eyes detection feature

--- app.py
+++ app.py
@@ -11,6 +11,19 @@ def webcam_frames():
         if not success:
             break   
         else:
+            detector = cv2.CascadeClassifier('Haarcascade_files\haarcascade_frontalface_default.xml')
+            eye_cascsde = cv2.CascadeClassifier('Haarcascade_files\haarcascade_eye.xml')
+            faces = detector.detectMultiScale(frame,1.1,7)
+            gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+            for (x,y,w,h) in faces:
+                cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = frame[y:y+h, x:x+w]
+                eyes = eye_cascsde.detectMultiScale(roi_gray, 1.1, 3)
+                for (ex,ey,ew,eh) in eyes:
+                    cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0,255,0), 2)
+
+
             ret,buffer = cv2.imencode('.jpg',frame)
             frame = buffer.tobytes()
 
